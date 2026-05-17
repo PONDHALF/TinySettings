c6e6031c7656e24a0db24a746c584c9de9f74bba
POND <private.pondhalf@gmail.com>
2026-05-17 15:14:22 +0700

    feat: implement TinySettings GUI framework with toggle persistence and dynamic menu system

---
+++ src/main/java/me/pondhalf/project/tinySettings/TinySettings.java
@@ -0,0 +1,17 @@
+package me.pondhalf.project.tinySettings;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TinySettings extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
